f6829b120fa55112d94be45c836ad02439adeecd
Dan Beers <spanyell@icloud.com>
2025-04-22 13:25:27 -0700

    Initial Commit

---
+++ AudioManager/AudioManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioManagerApp.swift
+//  AudioManager
+//
+//  Created by Dan Beers on 4/22/25.
+//
+
+import SwiftUI
+
+@main
+struct AudioManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AudioManagerTests/AudioManagerTests.swift
@@ -0,0 +1,17 @@
+//
+//  AudioManagerTests.swift
+//  AudioManagerTests
+//
+//  Created by Dan Beers on 4/22/25.
+//
+
+import Testing
+@testable import AudioManager
+
+struct AudioManagerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
